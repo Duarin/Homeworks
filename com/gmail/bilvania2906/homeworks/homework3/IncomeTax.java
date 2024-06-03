8e80872d9b450355c5ac8825f0ef3b4d240d0dfb
Duarin <bilvania2906@gmail.com>
2024-06-03 17:32:02 +0200

    Homework3

---
+++ com/gmail/bilvania2906/homeworks/homework3/IncomeTax.java
@@ -0,0 +1,23 @@
+package com.gmail.bilvania2906.homeworks.homework3;
+
+import java.util.Scanner;
+
+public class IncomeTax {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double tax;
+    System.out.println("Enter the amount of money income: ");
+    int incomeMoney= scanner.nextInt();
+    if (incomeMoney<=10000){
+        tax = incomeMoney * 0.025;
+        System.out.println("Tax is $: "+tax);
+    } else if (incomeMoney>10000&&incomeMoney<=25000) {
+        tax = incomeMoney* 0.043;
+        System.out.println("Tax is $: "+tax);
+    }else if (incomeMoney>25000){
+        tax = incomeMoney * 0.067;
+        System.out.println("Tax is $: "+tax);
+    }
+
+}
+}
