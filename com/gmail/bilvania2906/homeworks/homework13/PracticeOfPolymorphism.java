2f9f8bccccdeb019b88c808bc6d7f7838c78ce14
Duarin <bilvania2906@gmail.com>
2024-07-15 13:49:24 +0200

    Homework13

---
+++ com/gmail/bilvania2906/homeworks/homework13/PracticeOfPolymorphism.java
@@ -0,0 +1,68 @@
+package com.gmail.bilvania2906.homeworks.homework13;
+
+public class PracticeOfPolymorphism {
+
+    interface Shape {
+        double getArea();
+    }
+
+    static class Circle implements Shape {
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public double getArea() {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    static class Triangle implements Shape {
+        private double base;
+        private double height;
+
+        public Triangle(double base, double height) {
+            this.base = base;
+            this.height = height;
+        }
+
+        @Override
+        public double getArea() {
+            return 0.5 * base * height;
+        }
+    }
+
+    static class Square implements Shape {
+        private double side;
+
+        public Square(double side) {
+            this.side = side;
+        }
+
+        @Override
+        public double getArea() {
+            return side * side;
+        }
+    }
+
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(5);
+        shapes[1] = new Triangle(4, 6);
+        shapes[2] = new Square(7);
+
+        double totalArea = calculateTotalArea(shapes);
+        System.out.println("Сумарна площа всіх фігур: " + totalArea);
+    }
+
+    public static double calculateTotalArea(Shape[] shapes) {
+        double totalArea = 0;
+        for (Shape shape : shapes) {
+            totalArea += shape.getArea();
+        }
+        return totalArea;
+    }
+}
+
