254bbb067e13f9694cc86e943f0fc0a0ead4cd19
Duarin <bilvania2906@gmail.com>
2024-08-19 15:52:26 +0200

    convert miles in kilometers

--- com/gmail/bilvania2906/homeworks/homework15/Converter.java
+++ com/gmail/bilvania2906/homeworks/homework15/Converter.java
@@ -3,5 +3,13 @@ package com.gmail.bilvania2906.homeworks.homework15;
 public class Converter {
     public static void main(String[] args) {
         System.out.println("This program converts kilometers to miles");
+        System.out.println("10 miles in kilometers: " + milesToKilometers(10));
+
+
     }
+
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
+    }
+
 }
