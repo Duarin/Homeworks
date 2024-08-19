39e5e75be08d48ecc1c37eb50484f97cdcce0393
Duarin <bilvania2906@gmail.com>
2024-08-19 15:46:15 +0200

    First commit

---
+++ com/gmail/bilvania2906/homeworks/homework15/Converter.java
@@ -0,0 +1,7 @@
+package com.gmail.bilvania2906.homeworks.homework15;
+
+public class Converter {
+    public static void main(String[] args) {
+        System.out.println("This program converts kilometers to miles");
+    }
+}
