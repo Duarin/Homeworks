15f336c0d684429eb8c4977407e7f8d203f16df9
Duarin <bilvania2906@gmail.com>
2024-08-23 14:02:28 +0200

    rename files

--- com/gmail/bilvania2906/homeworks/homework16/Converter2.java
+++ com/gmail/bilvania2906/homeworks/homework16/Converter2.java
@@ -1,6 +1,6 @@
 package com.gmail.bilvania2906.homeworks.homework16;
 
-public class Converter {
+public class Converter2 {
     public static void main(String[] args) {
         System.out.println("This program converts fahrenheits to celsius");
         System.out.println("50  fahrenheits in celsius: " + fahrenheitsInCelsius(50));
