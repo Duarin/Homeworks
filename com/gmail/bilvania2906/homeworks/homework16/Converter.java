3f34d21104f2388311dd1ce64a8048bdf11d39a8
Duarin <bilvania2906@gmail.com>
2024-08-23 13:24:17 +0200

    the function of converting Fahrenheit to Celsius

--- com/gmail/bilvania2906/homeworks/homework16/Converter.java
+++ com/gmail/bilvania2906/homeworks/homework16/Converter.java
@@ -3,5 +3,9 @@ package com.gmail.bilvania2906.homeworks.homework16;
 public class Converter {
     public static void main(String[] args) {
         System.out.println("This program converts fahrenheits to celsius");
+        System.out.println("50  fahrenheits in celsius: " + fahrenheitsInCelsius(50));
+    }
+    public static double fahrenheitsInCelsius(double F) {
+        return (F-32)/1.8;
     }
 }
