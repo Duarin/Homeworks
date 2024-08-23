f3d9d61748d37382e14053541869b44d11a51440
Duarin <bilvania2906@gmail.com>
2024-08-23 14:16:28 +0200

    changes if file converter3, on branch develop

--- com/gmail/bilvania2906/homeworks/homework17/Converter3.java
+++ com/gmail/bilvania2906/homeworks/homework17/Converter3.java
@@ -1,4 +1,23 @@
 package com.gmail.bilvania2906.homeworks.homework17;
 
 public class Converter3 {
+    private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter app.");
+
+        double kgs = 5;
+        double pnds = 12;
+        double pounds = convKgsToPounds(kgs);
+        double kilos = convPoundsToKilos(pnds);
+        System.out.println("Result is: " + pounds + " pounds");
+    }
+
+    private static double convPoundsToKilos(double pnds) {
+        return pnds / CONV_K;
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
 }
