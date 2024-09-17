db7cf4942dda92bdb7a1919a3fb3f713d5d8ab24
Duarin <bilvania2906@gmail.com>
2024-09-17 18:08:47 +0200

    homework21

---
+++ com/gmail/bilvania2906/homeworks/homework21/FundsException.java
@@ -0,0 +1,11 @@
+package com.gmail.bilvania2906.homeworks.homework21;
+
+
+// Кастомний виняток
+public class FundsException extends Exception {
+
+    public FundsException(String message) {
+        super(message);
+    }
+
+}
