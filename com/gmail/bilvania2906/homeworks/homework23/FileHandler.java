a1f02b9d04ca6d33a1d924f58aca816a91d53b3e
Duarin <bilvania2906@gmail.com>
2024-10-01 14:21:07 +0200

    homework23

---
+++ com/gmail/bilvania2906/homeworks/homework23/FileHandler.java
@@ -0,0 +1,35 @@
+package com.gmail.bilvania2906.homeworks.homework23;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+
+    private final static String BASE_PATH = "files/";
+
+    public String writeFile(String fileName,String fileContent) {
+        try (FileWriter fw = new FileWriter (BASE_PATH + fileName + ".txt")) {
+            fw.write(fileContent);
+            return "Success.";
+        } catch (Exception e) {
+            return "Error:" + e.getMessage();
+        }
+    }
+
+    public String readFile(String path) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (FileReader reader = new FileReader(path)) {
+            int sym;
+            new StringBuilder();
+            while ((sym = reader.read()) != -1) {
+                stringBuilder.append((char) sym);
+            }
+            return path;
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+    }
+}
+
---
+++ com/gmail/bilvania2906/homeworks/homework23/Main.java
@@ -0,0 +1,27 @@
+package com.gmail.bilvania2906.homeworks.homework23;
+
+import java.io.IOException;
+
+public class Main {
+
+    private final static String BASE_PATH = "files/";
+
+    public static void main(String[] args) {
+        FileHandler handler = new FileHandler();
+
+        String fileName = "myfile";
+        String fileContent = "My very important information.";
+
+        // Запис файлу
+        String result = handler.writeFile(fileName, fileContent);
+        getOutput("RESULT: " + result);
+
+        // Читання файлу
+        String content = handler.readFile(BASE_PATH + fileName + ".txt");
+        getOutput("FILE CONTENT: " + content);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
