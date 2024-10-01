64a625260c36c78f772055bc410d912f15691128
Duarin <bilvania2906@gmail.com>
2024-10-01 14:33:17 +0200

    homework24

---
+++ com/gmail/bilvania2906/homeworks/homework24/FileHandler.java
@@ -0,0 +1,41 @@
+package com.gmail.bilvania2906.homeworks.homework24;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileHandler {
+
+    public String createFile(String path) {
+        Path newFile;
+        try {
+            newFile = Files.createFile(Paths.get(path));
+        } catch (FileAlreadyExistsException e) {
+            return "File already exists!";
+        } catch (IOException e) {
+            return "Something wrong " +e.getMessage();
+        }
+        return "Created " + newFile;
+    }
+
+    public String writeToFile(Path path, String content) {
+        try {
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        return "Recorded in " + path;
+    }
+
+    public String readFromFile(String path) {
+        try {
+            return Files.readString(Paths.get(path));
+        } catch (IOException e) {
+            return "Something wrong " + e.getMessage();
+        }
+    }
+}
+
+
