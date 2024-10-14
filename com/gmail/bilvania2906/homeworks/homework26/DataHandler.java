624af58600304c7c341977afd08cdc4aa4006734
Duarin <bilvania2906@gmail.com>
2024-10-14 15:58:35 +0200

    homework26

---
+++ com/gmail/bilvania2906/homeworks/homework26/DataHandler.java
@@ -0,0 +1,20 @@
+package com.gmail.bilvania2906.homeworks.homework26;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DataHandler {
+
+    private final Lock lock = new ReentrantLock();
+
+    public synchronized int modify(int num) {
+        lock.lock();
+        try {
+            num = num * 3;
+            return num;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
---
+++ com/gmail/bilvania2906/homeworks/homework26/DataRepository.java
@@ -0,0 +1,8 @@
+package com.gmail.bilvania2906.homeworks.homework26;
+
+public class DataRepository {
+
+    public int[] getData() {
+        return new int[] {7, 4, 5, 2};
+    }
+}
